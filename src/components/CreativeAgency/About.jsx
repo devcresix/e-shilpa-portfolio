ff3aec1d75ff08ad264ae22fbc4d41494868055c
Dev Cresix <52518140+devcresix@users.noreply.github.com>
2023-11-09 16:33:36 +0530

    Merge pull request #2 from devcresix/config header title change

--- src/components/CreativeAgency/About.jsx
+++ src/components/CreativeAgency/About.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React from "react";
 
 function About() {
   return (
-    <section className="tc-about-style5">
+    <section className="tc-about-style5" id="about">
       <div className="container">
         <div className="row gx-5">
           <div className="col-lg-6">
             <div className="img">
-              <img src="/home_5/assets/img/about.jpg" alt="" className="main-img wow zoomIn slow" />
+              <img
+                src="/home_5/assets/img/about.jpg"
+                alt=""
+                className="main-img wow zoomIn slow"
+              />
               <div className="rotate-box" data-speed="1" data-lag="0.7">
-                <a href="#" className="rotate-circle fsz-30 rotate-text d-inline-block text-uppercase">
+                <a
+                  href="#"
+                  className="rotate-circle fsz-30 rotate-text d-inline-block text-uppercase"
+                >
                   <svg className="textcircle" viewBox="0 0 500 500">
                     <defs>
-                      <path id="textcircle"
-                        d="M250,400 a150,150 0 0,1 0,-300a150,150 0 0,1 0,300Z">
-                      </path>
+                      <path
+                        id="textcircle"
+                        d="M250,400 a150,150 0 0,1 0,-300a150,150 0 0,1 0,300Z"
+                      ></path>
                     </defs>
                     <text>
-                      <textPath xlinkHref="#textcircle" textLength="900">Tech Adventures for Little Explorers. </textPath>
+                      <textPath xlinkHref="#textcircle" textLength="900">
+                        Tech Adventures for Little Explorers.{" "}
+                      </textPath>
                     </text>
                   </svg>
                 </a>
@@ -27,19 +37,41 @@ function About() {
           </div>
           <div className="col-lg-6">
             <div className="info ps-lg-5 js-splittext-lines">
-              <p className="fsz-14 text-uppercase color-666 mb-20"> About Us </p>
-              <h2 className="fsz-50 mb-40"> Unlocking the Future with ICT and Robotics</h2>
-              <div className="text fsz-16 color-666 mb-50">Cultivate innovation with our ICT and robotics education services. We empower students to embrace technology, fostering creativity and problem-solving for tomorrow's leaders.</div>
-              <a href="#" className="numb-card d-block pb-30 mb-30 border-1 border-bottom">
+              <p className="fsz-14 text-uppercase color-666 mb-20">
+                {" "}
+                About Us{" "}
+              </p>
+              <h2 className="fsz-50 mb-40">
+                {" "}
+                Unlocking the Future with ICT and Robotics
+              </h2>
+              <div className="text fsz-16 color-666 mb-50">
+                Cultivate innovation with our ICT and robotics education
+                services. We empower students to embrace technology, fostering
+                creativity and problem-solving for tomorrow's leaders.
+              </div>
+              <a
+                href="#"
+                className="numb-card d-block pb-30 mb-30 border-1 border-bottom"
+              >
                 <div className="row">
                   <div className="col-10">
                     <div className="cont d-flex">
-                      <h3 className="color-orange1 fsz-45 me-20 flex-shrink-0 fw-500"> 85% </h3>
-                      <p> happy customers <br /> & repeating </p>
+                      <h3 className="color-orange1 fsz-45 me-20 flex-shrink-0 fw-500">
+                        {" "}
+                        85%{" "}
+                      </h3>
+                      <p>
+                        {" "}
+                        happy customers <br /> & repeating{" "}
+                      </p>
                     </div>
                   </div>
                   <div className="col-2">
-                    <span className="arrow fsz-27 color-999"> <i className="ti-arrow-top-right"></i> </span>
+                    <span className="arrow fsz-27 color-999">
+                      {" "}
+                      <i className="ti-arrow-top-right"></i>{" "}
+                    </span>
                   </div>
                 </div>
               </a>
@@ -47,12 +79,21 @@ function About() {
                 <div className="row">
                   <div className="col-10">
                     <div className="cont d-flex">
-                      <h3 className="color-orange1 fsz-45 me-20 flex-shrink-0 fw-500"> 352+ </h3>
-                      <p> projects completed on <br /> over 20 countries </p>
+                      <h3 className="color-orange1 fsz-45 me-20 flex-shrink-0 fw-500">
+                        {" "}
+                        352+{" "}
+                      </h3>
+                      <p>
+                        {" "}
+                        projects completed on <br /> over 20 countries{" "}
+                      </p>
                     </div>
                   </div>
                   <div className="col-2">
-                    <span className="arrow fsz-27 color-999"> <i className="ti-arrow-top-right"></i> </span>
+                    <span className="arrow fsz-27 color-999">
+                      {" "}
+                      <i className="ti-arrow-top-right"></i>{" "}
+                    </span>
                   </div>
                 </div>
               </a>
@@ -61,7 +102,7 @@ function About() {
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
