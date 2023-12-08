abe7569e28164044aa8107abed2597897f8bdfdb
LakshithaMahen <129718328+LakshithaMahen@users.noreply.github.com>
2023-12-08 13:42:32 +0530

    Reviews

--- src/components/CreativeAgency/DiscussWrapper.jsx
+++ src/components/CreativeAgency/DiscussWrapper.jsx
@@ -5,14 +5,7 @@ function DiscussWrapper() {
     <section className="tc-discuss-style5">
       <div className="container">
         <div className="content">
-          <h2 className="fsz-40"> Discuss about notch project </h2>
-          <span className="line"></span>
-          <div className="button_su border-0">
-            <span className="su_button_circle bg-000 desplode-circle"></span>
-            <a href="#" className="butn text-uppercase border-1 border-white border button_su_inner bg-transparent text-white py-3 px-5">
-              <span className="button_text_container fsz-14"> join us today </span>
-            </a>
-          </div>
+          <h2 className="fsz-40"> Student Feedback </h2>
         </div>
       </div>
     </section>
